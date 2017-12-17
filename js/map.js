278687cb17f8f97449bbf2c90a4bf839d61486f1
irena-sh <ira.shvecova@gmail.com>
2017-12-17 12:29:18 +0200

    created show-card

--- js/map.js
+++ js/map.js
@@ -1,9 +1,6 @@
-﻿'use strict';
+'use strict';
 
 (function () {
-  var ESC_KEYCODE = 27;
-  var ENTER_KEYCODE = 13;
-
   var LIMIT = {
     min: 100,
     max: 660
@@ -14,15 +11,10 @@
 
   var mainMap = document.querySelector('.map');
   var pinsContainer = mainMap.querySelector('.map__pins');
-  var mapElementTemplate = document.querySelector('template').content.querySelector('.map__card');
-  var mapElement = mapElementTemplate.cloneNode(true);
   var pinMain = mainMap.querySelector('.map__pin--main');
-  var mapElementClose = mapElement.querySelector('.popup__close');
   var fragment = document.createDocumentFragment();
-  var actualPin = false;
   var allObjects = []; // объекты недвижимости
   var mainPin = document.querySelector('.map__pin--main');
-
 
   var startPage = function () { // начало работы с картой
     mainMap.classList.remove('map--faded');
@@ -30,55 +22,8 @@
     window.form.activate();
   };
 
-  var onPopupEscPress = function (event) {
-    if (event.keyCode === ESC_KEYCODE) {
-      closePopup();
-    }
-  };
-
-  var hidePin = function () {
-    if (actualPin !== false) {
-      actualPin.classList.remove('map__pin--active');
-    }
-  };
-
-  var openPopup = function () {
-    mapElement.classList.remove('hidden');
-    document.addEventListener('keydown', onPopupEscPress);
-  };
-
-  var closePopup = function () {
-    mapElement.classList.add('hidden');
-    hidePin();
-    actualPin = false;
-    document.removeEventListener('keydown', onPopupEscPress);
-  };
-
-  var closePinMouse = function () { // закрыть мышкой
-    closePopup();
-  };
-
-  var onEnterClosePin = function (event) { // закрыть клавишей
-    if (event.keyCode === ENTER_KEYCODE) {
-      closePopup();
-    }
-  };
-
-  var clickPin = function (event) {
-    var activeElement = event.target;
-    while (activeElement !== pinsContainer) {
-      if (activeElement.tagName === 'BUTTON') {
-        hidePin();
-        activeElement.classList.add('map__pin--active');
-        actualPin = activeElement;
-        if (!activeElement.classList.contains('map__pin--main')) {
-          window.card.render(mapElement, allObjects[activeElement.dataset.numPin]);
-          openPopup();
-        }
-        return;
-      }
-      activeElement = activeElement.parentNode;
-    }
+  var clickPin = function (evt) {
+    window.showCard.openCard(evt.target, allObjects, pinsContainer);
   };
 
   var onStartPageMousedown = function (event) {
@@ -120,12 +65,8 @@
   pinMain.addEventListener('mousedown', onStartPageMousedown);
   pinMain.addEventListener('mouseup', startPage);
   pinsContainer.addEventListener('click', clickPin);
-  mapElementClose.addEventListener('click', closePinMouse);
-  mapElementClose.addEventListener('keydown', onEnterClosePin);
-
 
   allObjects = window.data.getPosts();
   allObjects.forEach(window.pin.render, fragment);
-  mainMap.appendChild(mapElement);
-  mapElement.classList.add('hidden');
+  mainMap.appendChild(window.showCard.openCard(pinMain, allObjects[0], pinsContainer));
 })();
---
+++ js/show-card.js
@@ -0,0 +1,69 @@
+﻿'use strict';
+window.showCard = (function () {
+
+  var ESC_KEYCODE = 27;
+  var ENTER_KEYCODE = 13;
+
+  var mapElementTemplate = document.querySelector('template').content.querySelector('.map__card');
+  var mapElement = mapElementTemplate.cloneNode(true);
+  var mapElementClose = mapElement.querySelector('.popup__close');
+  var actualPin = false;
+
+  var onPopupEscPress = function (event) {
+    if (event.keyCode === ESC_KEYCODE) {
+      closePopup();
+    }
+  };
+
+  var hidePin = function () {
+    if (actualPin !== false) {
+      actualPin.classList.remove('map__pin--active');
+    }
+  };
+
+  var closePinMouse = function () { // закрыть мышкой
+    closePopup();
+  };
+
+  var onEnterClosePin = function (event) { // закрыть клавишей
+    if (event.keyCode === ENTER_KEYCODE) {
+      closePopup();
+    }
+  };
+
+  var openPopup = function () {
+    mapElement.classList.remove('hidden');
+    document.addEventListener('keydown', onPopupEscPress);
+  };
+
+  var closePopup = function () {
+    mapElement.classList.add('hidden');
+    hidePin();
+    actualPin = false;
+    document.removeEventListener('keydown', onPopupEscPress);
+  };
+
+  mapElementClose.addEventListener('click', closePinMouse);
+  mapElementClose.addEventListener('keydown', onEnterClosePin);
+
+  return {
+    openCard: function (elem, arrAds, pins) {
+      var activeElement = elem;
+      while (activeElement !== pins) {
+        if (activeElement.tagName === 'BUTTON') {
+          hidePin();
+          activeElement.classList.add('map__pin--active');
+          actualPin = activeElement;
+          if (!activeElement.classList.contains('map__pin--main')) {
+            window.card.render(mapElement, arrAds[activeElement.dataset.numPin]);
+            openPopup();
+          } else {
+            mapElement.classList.add('hidden');
+          }
+        }
+        activeElement = activeElement.parentNode;
+      }
+      return mapElement;
+    }
+  };
+})();
